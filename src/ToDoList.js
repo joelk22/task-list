27c66230b7148b4d267fda7a2aed2a2fb2937d4f
Joel <joelmkim382001@gmail.com>
2025-01-31 20:19:59 -0500

    Set up basic functions

---
+++ src/ToDoList.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function ToDoList() {
+
+    // tasks is set to an empty array with a setter
+    const [tasks, setTask] = useState([]);
+    // new task is what we will be editing and adding to task list
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+
+    }
+
+
+    return(<></>)
+}
+
+export default ToDoList
