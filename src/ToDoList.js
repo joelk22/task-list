8191c24861c935dd1adf6d81a6d1368c09f63163
joelk22 <joelmkim382001@gmail.com>
2025-02-03 16:03:34 -0500

    Code moveTaskDown function to properly move tasks down

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -33,7 +33,12 @@ function ToDoList() {
     }
 
     function moveTaskDown(index){
-
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] =
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
 
